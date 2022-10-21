39175ce9a0621b3eb841ce00dadaaca281139499
ssaid <94416699+swywssaid@users.noreply.github.com>
2022-10-21 14:23:27 +0900

    feat: Create logout
    - for logout, create logout button in landingpage
    - if user clicks the button, logout by axios
    - axios sends request to the endpoint of logout in server

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,13 +1,25 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   // get방식으로 /api/hello 로 req 날림,
   // 서버에서 돌아오는 response 콘솔에 찍음.
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
 
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃에 실패 했습니다.");
+      }
+    });
+  };
   return (
     <div
       style={{
@@ -18,7 +30,9 @@ function LandingPage() {
         height: "100vh",
       }}
     >
-      시작 페이지
+      <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
